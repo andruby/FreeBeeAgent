cdd85d9f54432985d4530160e23eae6adec867a2
andruby <andrew@bedesign.be>
2011-05-05 20:23:09 +0200

    Basic Klantenlisting controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @van = if params[:van] then Date.parse(params[:van]) else 1.year.ago.at_beginning_of_year.to_date end
+    @tot = if params[:tot] then Date.parse(params[:tot]) else 1.year.ago.at_end_of_year.to_date end
   end
 
 end
