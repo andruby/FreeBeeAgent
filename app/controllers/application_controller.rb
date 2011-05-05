73f573928aec83f1a21f8f110184c5eddfd5fe15
andruby <andrew@bedesign.be>
2011-05-05 22:43:47 +0200

    Basic Klantenlisting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter :authenticate_with_freeagent
+  before_filter :get_contacts
+  
+  def authenticate_with_freeagent
+    FreeagentApi.authenticate(current_user) if user_signed_in?
+  end
+  
+  def get_contacts
+    @contacts = Contact.find(:all)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   def index
     @van = if params[:van] then Date.parse(params[:van]) else 1.year.ago.at_beginning_of_year.to_date end
     @tot = if params[:tot] then Date.parse(params[:tot]) else 1.year.ago.at_end_of_year.to_date end
+    @minimum = params[:minimum].try(:to_i) || 250
+    @grouped_invoices = Invoice.between(@van, @tot).group_by(&:contact_id).reject do |c_id, invoices| 
+      invoices.inject(0) { |sum, i| sum + i.net_value } < @minimum
+    end
+    @invoices = @grouped_invoices.values.flatten
   end
 
 end
