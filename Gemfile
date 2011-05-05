4d3362deaad95cef86077b0b619a4ddec7c093a1
andruby <andrew@bedesign.be>
2011-05-05 18:51:27 +0200

    Basic Rails 3.1 beta1 app

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta1'
+gem 'sqlite3'
+
+# Asset template engines
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+gem 'devise'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freebeeagent::Application.config.secret_token = '65836c9d1080f3daa9c4901f2eb96006d62b4a5b41777b79f7b1c6e2aad6ef12e1e3414f45d86ea6e4a81fd39d896cf4746df0fabff0706148ef8044b602f4c8'
